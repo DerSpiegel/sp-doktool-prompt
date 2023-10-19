d6daa66bede66102b47b315fd6eb056be16c962c
Ralph Göstenmeier <ralph.goestenmeier.extern@spiegel.de>
2023-10-19 11:45:03 +0200

    add postman doc

--- function_app.py
+++ function_app.py
@@ -5,24 +5,23 @@ import os
 import azure.functions as func
 from azure.cosmos import CosmosClient
 from azure.cosmos.partition_key import PartitionKey
+from requests import Response
 
 COSMOS_URL = os.environ.get("COSMOS_URL")
 COSMOS_KEY = os.environ.get("COSMOS_KEY")
-DATABASE_ID = os.environ.get("COSMOS_DATABASE")
-CONTAINER_ID = os.environ.get("COSMOS_CONTAINER")
+COSMOS_DATABASE = os.environ.get("COSMOS_DATABASE")
+COSMOS_CONTAINER = os.environ.get("COSMOS_CONTAINER")
+
+STATE_ERROR = "ERROR"
+STATE_OK = "OK"
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-
-def db_properties(database):
-    properties = database.read()
-    print(json.dumps(properties, indent=True))
 
 def items_read(container, max_item_count=100):
     items = list(container.read_all_items(max_item_count))
 
     return items
-
 
 def item_create(container, item):
     response = container.create_item(body=item)
@@ -68,6 +67,79 @@ def items_query(container,  doc_id = None, query="SELECT * FROM r"):
 
     return response
 
+#
+#
+#
+def checkConfig():
+    config = {}
+
+    config = {
+        "COSMOS_URL": COSMOS_URL,
+        # "COSMOS_KEY": COSMOS_KEY,
+        "COSMOS_DATABASE": COSMOS_DATABASE,
+        "COSMOS_CONTAINER": COSMOS_CONTAINER,
+    }
+
+    reason = ""
+    if COSMOS_URL is None:
+        reason = " COSMOS_URL is missing"
+
+    if COSMOS_KEY is None:
+        reason = " COSMOS_KEY is missing"
+
+    if COSMOS_DATABASE is None:
+        reason = " COSMOS_DATABASE is missing"
+
+    if COSMOS_CONTAINER is None:
+        reason = " COSMOS_CONTAINER is missing"
+
+    if reason:
+        return STATE_ERROR, config
+
+    return STATE_OK, config
+
+#
+#
+#
+def handleError(response: Response = None, message: str = None, exception: Exception = None, config = None):
+    result = {}
+    result["status"] = STATE_ERROR
+
+    if message is not None and message != "":
+        result["message"] = message
+    elif response is not None:
+        result["message"] = response['error']['message']
+
+    if exception is not None:
+        result["exception"] = str(exception)
+
+    if response is not None:
+        result["response"] = response
+
+    if config is not None:
+        result["config"] = config
+
+    return func.HttpResponse(json.dumps(result, indent=4), status_code=200)
+
+def handleResponse(response: Response = None, message: str = None, additional = None, config = None):
+    """
+
+    """
+    result = {}
+    result["status"] = STATE_OK
+
+    if message is not None and message != "":
+        result["message"] = message
+
+    result["responses"] = response
+
+    if additional is not None:
+        result.update(additional)
+
+    if config is not None:
+        result["config"] = config
+
+    return func.HttpResponse(json.dumps(result, indent=4), status_code=200)
 
 #
 #
@@ -76,6 +148,12 @@ def items_query(container,  doc_id = None, query="SELECT * FROM r"):
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("DokTool - Prompts Management")
 
+    state, config = checkConfig()
+
+    if state == STATE_ERROR:
+        return handleError(message = "Error in  configuration", config=config)
+
+    #
     id = req.params.get("id")
 
     try:
@@ -84,9 +162,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         pass
 
     #
-    client: CosmosClient = CosmosClient(COSMOS_URL, {"masterKey": COSMOS_KEY})
-    db = client.create_database_if_not_exists(id=DATABASE_ID)
-    container = db.create_container_if_not_exists(id=CONTAINER_ID, partition_key=PartitionKey(path='/id', kind='Hash'))
+    try:
+        client: CosmosClient = CosmosClient(COSMOS_URL, {"masterKey": COSMOS_KEY})
+        db = client.create_database_if_not_exists(id=COSMOS_DATABASE)
+        container = db.create_container_if_not_exists(id=COSMOS_CONTAINER, partition_key=PartitionKey(path='/id', kind='Hash'))
+    except Exception as ex:
+        return handleError(message = "Couild not connect to database", exception=ex, config=config)
+
+
 
     method = req.method
     logging.info(f"DokTool - Prompts Management: handle '{method}")
@@ -111,5 +194,5 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             items = {}
 
     #
-    result = {"method": method, "data": items}
-    return func.HttpResponse(json.dumps(result, indent=4), status_code=200)
+    additional = {"method": method}
+    return handleResponse(items, additional=additional, config=config)
